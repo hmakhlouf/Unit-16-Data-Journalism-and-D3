431759a9247fce188fe034c58b0f9798c354fb1c
Hocine Makhlouf <datamakhlouf@gmail.com>
2019-06-25 17:11:51 -0500

    index

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,15 +3,15 @@
 
 
 // Define SVG area dimensions
-var svgWidth = 1000;
-var svgHeight = 660;
+var svgWidth = 970;
+var svgHeight = 500;
 
 // Define the chart's margins as an object
 var chartMargin = {
   top: 30,
-  right: 30,
-  bottom: 30,
-  left: 30
+  right: 40,
+  bottom: 80,
+  left: 100
 };
 
 // Define dimensions of the chart area
@@ -20,7 +20,7 @@ var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
 // Select body, append SVG area to it, and set the dimensions
 var svg = d3
-  .select("body")
+  .select("#scatter")
   .append("svg")
   .attr("height", svgHeight)
   .attr("width", svgWidth);
@@ -31,13 +31,116 @@ var chartGroup = svg.append("g")
 .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
 // Load data from ther csv file 
-d3.csv("assets/data/data.csv", function(error, journalismData) {
+d3.csv("assets/data/data.csv")
+  .then(function(journalismData) {
 
 // Log an error if one exists
-if (error) return console.warn(error);
+// if (error) return console.warn(error);
 
 
 // Print the tvData
 console.log(journalismData);
 
+
+// Step 1: Parse Data/Cast as numbers
+    // ==============================
+    journalismData.forEach(function(data) {
+      data.healthcare = +data.healthcare;
+      data.age = +data.age;
+
+    })
+
+
+// Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([29, d3.max(journalismData, d => d.age)])
+      .range([0, chartWidth]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(journalismData, d => d.healthcare)])
+      .range([chartHeight, 0]);
+
+
+
+ // Step 3: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${chartHeight})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+ // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(journalismData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.age))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "15")
+    .attr("fill", "blue")
+    .attr("opacity", "0.6");
+
+
+    // Step 6: label the Circles
+
+    var circlesLabel = chartGroup.selectAll("state")
+    .data(journalismData)
+    .enter()
+    .append("text")
+    .attr("x", d => xLinearScale(d.age)-10)
+    .attr("y", d => yLinearScale(d.healthcare)+4)
+    .attr("fill", "white")
+    .text(d => d.abbr); 
+
+
+// Step 7: Initialize tool tip
+    // ==============================
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(function(d) {
+        return (`${d.state}<br> Age: ${d.age}<br>Healthcare: ${d.healthcare}`);
+      });
+
+    // Step 8: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
+
+// Step 9: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("click", function(data) {
+      toolTip.show(data, this);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - chartMargin.left +50)
+      .attr("x", 0 - (chartHeight / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Healthcare (%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 10})`)
+      .attr("class", "axisText")
+      .text("Age (Median)");
+ 
+
+
 }); 
+
+
